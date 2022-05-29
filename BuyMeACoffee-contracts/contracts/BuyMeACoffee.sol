780548fc2cd8535139fab3cdebce9dfb626d6cef
MSBivens <mikebiv95@gmail.com>
2022-05-29 08:49:57 -0500

    built contract

--- BuyMeACoffee-contracts/contracts/BuyMeACoffee.sol
+++ BuyMeACoffee-contracts/contracts/BuyMeACoffee.sol
@@ -18,6 +18,54 @@ contract BuyMeACoffee {
         string message;
     }
 
+    // List of all memos received from friends
+    Memo[] memos;
+
     // Address of contract deployer 
     address payable owner;
+
+    // Deploy Logic
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    /**
+     * @dev buy a coffee for contract owner
+     * @param _name name of the coffee buyer
+     * @param _message a message from the coffee buyer
+     */
+    function buyCoffee(string memory _name, string memory _message) public payable {
+        require(msg.value > 0, "can't buy a coffee with 0 eth");
+
+        // adds the memo to storage
+        memos.push(Memo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        ));
+
+        // Emit a log event when a new memo is created
+        emit NewMemo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        );
+    }
+
+    /**
+     * @dev send the entire balance stored in this contract to the owner
+     */
+    function withdrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+
+    /**
+     * @dev retrievs all the memos received and stored on the blockchain
+     */
+    function getMemos() public view returns(Memo[] memory) {
+        return memos;
+    }
+
 }
